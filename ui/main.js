ecb80ab7294b45636f9392ec46af2e3d67ee4566
salGitHub <sal9447523429@gmail.com>
2016-10-24 23:24:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 // Change the text of the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+// Move the image
+var img=document.getElementById('madi');
+img.click=function() {
+    img.style.marginleft='100px';
+    
+}
